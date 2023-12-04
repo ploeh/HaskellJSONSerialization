9382669db722d7a8d658bab039b974d302569180
Mark Seemann <mark@ploeh.dk>
2023-12-04 15:27:27 +0100

    Add test of serialization

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,9 +1,17 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
+import Data.Aeson
+import Restaurants
+import JSONSerialization
 import Test.HUnit
 import Test.Framework.Providers.HUnit (hUnitTestToTests)
 import Test.Framework (defaultMain)
 
 main :: IO ()
 main = defaultMain $ hUnitTestToTests $ TestList [
+  "Serialize communal table for 42" ~:
+    let table = tryCommunalTable 42
+        actual = encode . JSONTable <$> table
+    in Just "{\"communalTable\":{\"capacity\":42}}" ~=? actual
   ]
