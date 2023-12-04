184672cd33d9dd3bbfc951f1ef5e5ee7fb308221
Mark Seemann <mark@ploeh.dk>
2023-12-04 12:28:43 +0100

    Add Restaurant API

---
+++ src/Restaurants.hs
@@ -0,0 +1,23 @@
+module Restaurants (tryNatural, trySingleTable, tryCommunalTable) where
+
+newtype Natural = Natural Integer deriving (Eq, Ord, Show)
+
+tryNatural :: Integer -> Maybe Natural
+tryNatural n
+  | n < 1 = Nothing
+  | otherwise = Just (Natural n)
+
+data SingleTable = SingleTable
+  { singleCapacity :: Natural
+  , minimalReservation :: Natural
+  } deriving (Eq, Ord, Show)
+data Table = Single SingleTable | Communal Natural deriving (Eq, Show)
+
+trySingleTable :: Integer -> Integer -> Maybe SingleTable
+trySingleTable capacity minimal = do
+  c <- tryNatural capacity
+  m <- tryNatural minimal
+  if c < m then Nothing else Just (SingleTable c m)
+
+tryCommunalTable :: Integer -> Maybe Table
+tryCommunalTable = fmap Communal . tryNatural
