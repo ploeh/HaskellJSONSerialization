e8c5607d4d9ced372de94095be24f2b0c64edfa8
Mark Seemann <mark@ploeh.dk>
2023-12-06 09:22:44 +0100

    Use QuasiQuotes for JSON strings

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,6 +1,8 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes #-}
 module Main where
 
+import Text.RawString.QQ(r)
 import Restaurants
 import JSONSerialization
 import Test.HUnit
@@ -12,20 +14,20 @@ main = defaultMain $ hUnitTestToTests $ TestList [
   "Serialize communal table for 42" ~:
     let table = tryCommunalTable 42
         actual = serializeTable <$> table
-    in Just "{\"communalTable\":{\"capacity\":42}}" ~=? actual
+    in Just [r|{"communalTable":{"capacity":42}}|] ~=? actual
   ,
   "Serialize single table for 2" ~:
     let table = trySingleTable 2 1
         actual = serializeTable <$> table
-    in Just "{\"singleTable\":{\"capacity\":2,\"minimalReservation\":1}}" ~=? actual
+    in Just [r|{"singleTable":{"capacity":2,"minimalReservation":1}}|] ~=? actual
   ,
   "Deserialize communal table for 42" ~:
-    let json = "{\"communalTable\":{\"capacity\":42}}"
+    let json = [r|{"communalTable":{"capacity":42}}|]
         actual = tryDeserializeTable json
     in tryCommunalTable 42 ~=? actual
   ,
   "Deserialize single table for 4" ~:
-    let json = "{\"singleTable\":{\"capacity\":4,\"minimalReservation\":3}}"
+    let json = [r|{"singleTable":{"capacity":4,"minimalReservation":3}}|]
         actual = tryDeserializeTable json
     in trySingleTable 4 3 ~=? actual
   ]
